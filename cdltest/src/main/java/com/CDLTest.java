2d8b3ad232f97c43318ae011928548e8828ff2ce
jirimutu <jirimutu007@163.com>
2017-04-12 14:41:54 +0800

    话单测试类。

---
+++ cdltest/src/main/java/com/CDLTest.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by 吉日木图 on 2017/4/12.
+ */
+public class CDLTest {
+    public static void main(String[] args) {
+        System.out.println("print CDL result data!");
+    }
+}
---
+++ cdltest/src/main/java/com/boco.java
@@ -0,0 +1,7 @@
+package com;
+
+/**
+ * Created by 吉日木图 on 2017/4/12.
+ */
+public class boco {
+}
